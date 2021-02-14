34bfca72fa20411f80088b488f17ceda4bb95de1
Jenny Aguilar <contact@jaguilarweb.com>
2021-02-14 12:05:38 -0300

    refactor: Change structure app file database connection

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import os
 from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from models import setup_db
 from flask_cors import CORS
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-  return app
+
+    @app.route('/')
+    def index():
+      gretting = "Hello Word"
+      return gretting
+
+
+
+    return app
+
 
 APP = create_app()
 
