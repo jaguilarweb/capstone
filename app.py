f8ba533b93445d2393681a1257bec5b7c04b92f0
Jenny Aguilar <contact@jaguilarweb.com>
2021-02-14 18:27:15 -0300

    fix: Fix bug in manage and app files

--- app.py
+++ app.py
@@ -15,12 +15,18 @@ def create_app(test_config=None):
       gretting = "Hello Word"
       return gretting
 
-
-
     return app
 
 
-APP = create_app()
+app = create_app()
 
+# Default port:
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run()
+
+# Or specify port manually:
+'''
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
