11bfc3da519255f22c434c5aa3b5bba1ad961711
Jenny Aguilar <contact@jaguilarweb.com>
2021-02-16 11:31:35 -0300

    refactor: Add methods and changes tables atributes names

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import Column, Integer, Float, DateTime, String, ForeignKey, create_engine
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import sys
 import json
 
 
@@ -16,21 +17,73 @@ def setup_db(app, database_path=database_path):
     db.app = app
     db.init_app(app)
     db.create_all()
-    
+
 
 class Project(db.Model):
-
+    __tablename__ = 'project'
     id = Column(Integer, primary_key=True)
     name = Column(String)
     kind = Column(String)
-    delivery = Column(DateTime)
-    words = Column(Integer)
-    hour = Column(Float)
+    deadline = Column(DateTime)
+    word_count = Column(Integer)
+    hour_count = Column(Float)
     rate = Column(Float)
     person_id = Column(Integer, ForeignKey('person.id'))
     service_id = Column(Integer, ForeignKey('service.id'))
     person_child = db.relationship("Person", back_populates='services')
     service_child = db.relationship("Service", back_populates='people')
+
+    def __init__(self, name, kind, deadline, word_count, hour_count, rate, person_id, service_id):
+        self.name = name
+        self.kind = kind
+        self.deadline = deadline
+        self.word_count = word_count
+        self.hour_count = hour_count
+        self.rate = rate
+        self.person_id = person_id
+        self.service_id = service_id
+
+    def insert(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
+    def update(self):
+        try:
+            db.session.commit()
+        except:
+            db.session.rollback()
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
+    def format(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'kind': self.kind,
+            'deadline': self.deadline,
+            'word_count': self.word_count,
+            'hour_count': self.hour_count,
+            'rate': self.rate,
+            'person_id': self.person_id,
+            'service_id': self.service_id
+        }
 
 
 class Person(db.Model):
@@ -42,11 +95,56 @@ class Person(db.Model):
     email = Column(String)
     ratew = Column(Float)
     rateh = Column(Float)
-    
     services = db.relationship("Projects", back_populates="person_child")
-    
-    
-    
+
+    def __init__(self, name, kind, email, ratew, rateh):
+        self.name = name
+        self.kind = kind
+        self.email = email
+        self.ratew = ratew
+        self.rateh = rateh
+
+    def insert(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
+    def update(self):
+        try:
+            db.session.commit()
+        except:
+            db.session.rollback()
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
+    def format(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'kind': self.kind,
+            'email': self.email,
+            'ratew': self.ratew,
+            'rateh': self.rateh,
+            'service': self.services
+        }
+
+
 class Service(db.Model):
     __tablename__ = 'service'
 
@@ -54,5 +152,46 @@ class Service(db.Model):
     name = Column(String)
     source = Column(String)
     destiny = Column(String)
-    
     people = db.relationship("Projects", back_populates="service_child")
+
+    def __init__(self, name, source, destiny):
+        self.name = name
+        self.source = source
+        self.destiny = destiny
+
+    def insert(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
+    def update(self):
+        try:
+            db.session.commit()
+        except:
+            db.session.rollback()
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            print(sys.exc_info())
+        finally:
+            db.session.close()
+
+    def format(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'source': self.source,
+            'destiny': self.destiny
+        }
