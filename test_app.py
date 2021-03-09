240a47cec2be059323c9da5831e15825d1e8e2bd
Jenny Aguilar <contact@jaguilarweb.com>
2021-03-09 11:12:54 -0300

    test: Add Unittest basic structure

--- test_app.py
+++ test_app.py
@@ -1,1 +1,30 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
 
+from app import app
+from models import *
+
+
+class CapstoneTestCase(unittest.TestCase):
+    """This class represents the ___ test case"""
+
+    def setUp(self):
+        """Executed before each test. Define test variables and initialize app."""
+        self.client = app.test_client
+        pass
+
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    def test_given_behavior(self):
+        """Test _____________ """
+        res = self.client().get('/')
+
+        self.assertEqual(res.status_code, 200)
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+unittest.main()
