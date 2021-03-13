a125a1b5b681f5ffe782790fa4a8416866297586
Jenny Aguilar <contact@jaguilarweb.com>
2021-03-13 19:30:06 -0300

    fix: Change require_auth method

--- auth.py
+++ auth.py
@@ -143,18 +143,15 @@ def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
-            try:                
-                if session['token']:
-                    token = session['token']
-                else:
-                    token = get_token_auth_header()
-                if token is None: 
-                    abort(401)
+            token = get_token_auth_header()
+            try:
                 payload = verify_decode_jwt(token)
-                check_permissions(permission, payload)
-                return f(payload, *args, **kwargs)
             except:
                 abort(401)
-                
+            try:
+                check_permissions(permission, payload)
+            except:
+                abort(401)
+            return f(payload, *args, **kwargs)
         return wrapper
     return requires_auth_decorator
